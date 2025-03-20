3c118eebdc00887851f1b2545129fe10dbdf9243
bahareh <baharehviani@gmail.com>
2025-03-20 16:21:06 +0330

    small fixes

--- js/forms.js
+++ js/forms.js
@@ -1,13 +1,8 @@
-// JavaScript for handling the form submission
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission
+    event.preventDefault(); 
     
-    // Here you could add further validation or send an AJAX request
-    
-    // Alert to let the user know the form has been submitted
     alert("Your request has been submitted successfully!");
     
-    // Optionally, reset the form after submission
     document.getElementById("requestForm").reset();
     
     return false;
