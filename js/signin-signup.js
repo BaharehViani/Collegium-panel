3f443ee65d171b9513a60446697e396ab08b1448
bahareh <baharehviani@gmail.com>
2025-03-20 21:05:33 +0330

    login & sign-up functions added

--- js/signin-signup.js
+++ js/signin-signup.js
@@ -17,8 +17,52 @@ sign_in_btn.addEventListener("click", () => {
 });
 
 
-document.getElementById("numberInput").addEventListener("input", function () {
-    if (this.value.length > 10) {
-        this.value = this.value.slice(0, 10);
+
+const signupForm = document.getElementById("signup-form");
+signupForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    // گرفتن مقادیر ورودی‌ها
+    const fullName = document.querySelector('.sign-up-form input[placeholder="FullName"]').value;
+    const username = document.querySelector('.sign-up-form input[placeholder="Username"]').value;
+    const password = document.querySelector('.sign-up-form input[placeholder="Password"]').value;
+
+    try {
+        const response = await axios.post("http://localhost:3000/api/users/register", {
+            full_name: fullName,
+            username: username,
+            password: password
+        });
+
+        alert(response.data.message); // پیام موفقیت
+        window.location.href = "?mode=signin"; // بعد از ثبت‌نام، به صفحه لاگین بره
+    } catch (error) {
+        alert(error.response.data.message || "Registration failed!"); // نمایش ارور
+    }
+});
+
+
+const loginForm = document.querySelector(".sign-in-form");
+// افزودن event listener به فرم
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault(); // جلوگیری از ارسال فرم به صورت پیش‌فرض
+
+    const username = document.querySelector(".sign-in-form input[type='text']").value;
+    const password = document.querySelector(".sign-in-form input[type='password']").value;
+
+    try {
+        // ارسال درخواست به سرور برای ورود
+        const response = await axios.post('http://localhost:3000/api/users/login', {
+        username,
+        password
+        });
+
+        // در صورت موفقیت
+        console.log(response.data);
+        alert("Login successful");
+        window.location.href = "../html/dashboard.html";
+    } catch (error) {
+        console.error("Login failed:", error);
+        alert("Invalid username or password");
     }
 });
